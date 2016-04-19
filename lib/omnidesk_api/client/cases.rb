fccb4a2214d6886dc802e8956c9a5e5bef1b707b
V.Kolesnikov <re.vkolesnikov@gmail.com>
2016-04-19 14:50:21 +0300

    Add more methods to Client::Cases

--- lib/omnidesk_api/client/cases.rb
+++ lib/omnidesk_api/client/cases.rb
@@ -9,8 +9,36 @@ module OmnideskApi
         get "cases/#{case_id}.json", options
       end
 
+      def create_case(options = {})
+        post 'cases.json', options
+      end
+
+      def update_case(case_id, options = {})
+        put "cases/#{case_id}.json", options
+      end
+
+      def trash_case(case_id, options = {})
+        put "cases/#{case_id}/trash.json", options
+      end
+
+      def spam_case(case_id, options = {})
+        put "cases/#{case_id}/spam.json", options
+      end
+
+      def restore_case(case_id, options = {})
+        put "cases/#{case_id}/restore.json", options
+      end
+
+      def delete_case(case_id, options = {})
+        delete "cases/#{case_id}.json", options
+      end
+
       def messages(case_id, options = {})
         get "cases/#{case_id}/messages.json", options
+      end
+
+      def create_message(case_id, options = {})
+        post "cases/#{case_id}/messages.json", options
       end
 
       def cases_count(options = {})
