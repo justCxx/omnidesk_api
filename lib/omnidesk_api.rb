de13494dc07680705f30bdb1e7eff241fed24fcf
V.Kolesnikov <re.vkolesnikov@gmail.com>
2016-04-26 19:45:51 +0300

    Improve code style

--- lib/omnidesk_api.rb
+++ lib/omnidesk_api.rb
@@ -22,8 +22,8 @@ module OmnideskApi
     end
 
     def method_missing(method_name, *args, &block)
-      return client.send(method_name, *args, &block) if client.respond_to?(method_name)
-      super
+      return super unless client.respond_to?(method_name)
+      client.send(method_name, *args, &block)
     end
   end
 end
